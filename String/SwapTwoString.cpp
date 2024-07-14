68cb184e5d86bb663eebfb9c08b55f0819414a7e
Raunak85 <raunakraj838@gmail.com>
2024-07-14 10:25:30 +0530

    swapTwoString

---
+++ String/SwapTwoString.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+void SwapTwoString(string &s1,string &s2){
+    
+    string temp = s1;
+    s1 = s2;
+    s2 = temp;
+}
+
+int main(){
+    string s1="Raunak";
+    string s2="kumar";
+    SwapTwoString(s1,s2);
+    cout<<"s1 = "<<s1<<" s2 = "<<s2;
+}
