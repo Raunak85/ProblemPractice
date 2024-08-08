f0105e425545a5895eefc4d3609e96b3dfd05c94
Raunak85 <raunakraj838@gmail.com>
2024-08-08 17:18:06 +0530

    Factorial

--- Recursion/Factorial.cpp
+++ Recursion/Factorial.cpp
@@ -4,7 +4,9 @@ int Factorial(int n){
     if(n == 1 || n == 0){
         return 1;
     }
-    return n*Factorial(n-1);
+    int fact = Factorial(n-1);
+    int result = fact*n;
+    return result;
 }
 int main(){
     int n = 4;
