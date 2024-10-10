2c9dd407f6bb69e279ce54585844e506f5caca55
Raunak85 <raunakraj838@gmail.com>
2024-10-10 07:43:51 +0530

    Level Order Traversal(Binary Tree)

--- Tree/Binary Tree/LevelOrderTraversal_BFS.cpp
+++ Tree/Binary Tree/LevelOrderTraversal_BFS.cpp
@@ -35,25 +35,63 @@ Node *buildTree(Node *root)
 
     return root;
 }
+    //Direct print method
+// void levelOrderTraversal(Node* root){
+//     queue<Node*> q;
+//     q.push(root);
+//     while(!q.empty()){
+//         Node* temp = q.front();
+//         cout<<temp->data<<" ";
+//         q.pop();
+//         if(temp->left){
+//             q.push(temp->left);
+//         }
+//         if(temp->right){
+//             q.push(temp->right);
+//         }
+//     }
+// }
+
 
 void levelOrderTraversal(Node* root){
     queue<Node*> q;
     q.push(root);
+    q.push(nullptr);
     while(!q.empty()){
         Node* temp = q.front();
-        cout<<temp->data<<" ";
         q.pop();
-        if(temp->left){
-            q.push(temp->left);
-        }
-        if(temp->right){
-            q.push(temp->right);
+        if(temp == nullptr){
+            cout<<endl;
+            if(!q.empty()){
+                q.push(nullptr);
+            }
+        }else{
+            cout<<temp->data<<" ";
+            if(temp->left){
+                q.push(temp->left);
+            }
+            if(temp->right){
+                q.push(temp->right);
+            }
         }
     }
 }
+
+int leafNodeCount(Node* root,int &count){
+    if(root == nullptr) return 0;
+    if(root->left == nullptr && root->right == nullptr){
+        count++;
+    }
+    leafNodeCount(root->left,count);
+    leafNodeCount(root->right,count);
+    return count;
+}
+
 int main()
 {
     Node *root = nullptr;
     root = buildTree(root);
-    levelOrderTraversal(root);
+    // levelOrderTraversal(root);
+    int count = 0;
+    cout<<leafNodeCount(root,count);
 }
